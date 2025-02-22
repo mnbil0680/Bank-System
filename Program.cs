d007614a26cc0070e1473cb9ac9235402c9963bd
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-02-22 19:00:01 +0200

    finish the base code

--- Program.cs
+++ Program.cs
@@ -8,13 +8,34 @@ namespace Bank_System
     {
         static void Main(string[] args)
         {
-            
+            clsBankClient Client1 = clsBankClient.Find("A101");  // Find client by account number
 
-                
+            if (!Client1.IsEmpty())
+            {
+                Console.WriteLine("\nClient Found :-)\n");
+            }
+            else
+            {
+                Console.WriteLine("\nClient Was not Found :-(\n");
+            }
 
+            Client1.Print();  // Print client details
 
-               
-         
+            clsBankClient Client2 = clsBankClient.Find("A101", "1234"); // Find client with account number & password
+
+            if (!Client2.IsEmpty())
+            {
+                Console.WriteLine("\nClient Found :-)\n");
+            }
+            else
+            {
+                Console.WriteLine("\nClient Was not Found :-(\n");
+            }
+
+            Client2.Print();
+
+            Console.WriteLine("\nIs Client Exist? " + clsBankClient.IsClientExist("A101"));
+
         }
     }
 }
